57d51a61e031bdc4b39fa58ae9cb8a48729ee475
Robert Kolski <robert.kolski@outlook.com>
2019-08-05 22:37:50 -0700

    completed the shortcut version

---
+++ src/stablemarriage.core/Person.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stablemarriage.core
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, Gender gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = gender;
+            this.preferenceList = new SortedList<int, Person>();
+        }
+
+        public delegate void GetBackInLine(Person p, Person rejector);
+
+        public event GetBackInLine BackInTheGame;
+
+        private SortedList<int, Person> preferenceList;
+        private Gender gender;
+
+        private string firstName;
+        private string lastName;
+
+        private Person currentEngagement;
+        private int currentEngagementPreference;
+
+        public override string ToString()
+        {
+            //return string.Format("Id: {0}, First: {1}, Last: {2}, Gender: {3}", uniqueId, firstName, lastName, gender);
+            return string.Format("First: {0}, Last: {1}, Gender: {2}", firstName, lastName, gender);
+        }
+
+        public string FirstName { get { return firstName; } }
+
+        public string LastName { get { return lastName; } }
+
+        public Gender Gender { get { return gender; } }
+
+        public Person CurrentEngagement
+        {
+            get
+            {
+                return currentEngagement;
+            }
+            set
+            {
+                currentEngagement = value;
+            }
+        }
+
+        public void AddToList(Person p)
+        {
+            preferenceList.Add(preferenceList.Count + 1, p);
+        }
+
+        public Person GetNumberOne()
+        {
+            return preferenceList.First().Value;
+        }
+
+        public void Rejected(Person p)
+        {
+            preferenceList.RemoveAt(0);
+            this.BackInTheGame(this, p);
+        }
+
+        public bool HasAtLeastOneOption()
+        {
+            return preferenceList.Count > 0;
+        }
+
+        public Answer WillYouMarryMe(Person p)
+        {
+            if (this.gender == Gender.Female && p.gender == Gender.Male)
+            {
+                foreach (KeyValuePair<int, Person> option in this.preferenceList)
+                {
+                    if (PersonComparer.Instance.Equals(p, option.Value))
+                    {
+                        if (currentEngagement == null)
+                        {
+                            if (option.Key == this.preferenceList.Keys.First())
+                            {
+                                currentEngagement = p;
+                                currentEngagementPreference = option.Key;
+                                return Answer.Yes;
+                            }
+                            else
+                            {
+                                currentEngagement = p;
+                                currentEngagementPreference = option.Key;
+                                return Answer.Maybe;
+                            }
+                        }
+                        else
+                        {
+                            if (option.Key == this.preferenceList.Keys.First())
+                            {
+                                currentEngagement.Rejected(this);
+                                currentEngagement = p;
+                                currentEngagementPreference = option.Key;
+                                return Answer.Yes;
+                            }
+                            else if (currentEngagementPreference < option.Key)
+                            {
+                                p.Rejected(this);
+                                return Answer.No;
+                            }
+                            else
+                            {
+                                currentEngagement.Rejected(this);
+                                currentEngagement = p;
+                                currentEngagementPreference = option.Key;
+                                return Answer.Maybe;
+                            }
+                        }
+                    }
+                }
+            }
+            throw new InvalidOperationException("Men propose to women.");
+        }
+    }
+}
---
+++ src/stablemarriage/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using stablemarriage.core;
+
+namespace stablemarriage
+{
+    class Program
+    {
+        static List<Person> singleMen = new List<Person>();
+        static List<Person> unmarriedWomen = new List<Person>();
+        static List<Person> unmarriedMen = new List<Person>();
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                System.Console.Out.WriteLine("usage stablemarriage masterlist.txt");
+                return;
+            }
+
+            ListReader listReader = new ListReader();
+            List<Person> population = listReader.GetPopulation(args[0]).ToList();
+            List<Person> men = population.Where(x => x.Gender == Gender.Male).ToList();
+            men.ForEach(x => x.BackInTheGame += GetBackInTheGame);
+            List<Person> women = population.Where(x => x.Gender == Gender.Female).ToList();
+            unmarriedWomen = women.ToList();
+
+            singleMen = men.ToList();
+            while (singleMen.Count > 0)
+            {
+                Person currentMan = singleMen.First();
+                Person favoredWoman = currentMan.GetNumberOne();
+                if (favoredWoman != null)
+                {
+                    Answer answer = favoredWoman.WillYouMarryMe(currentMan);
+                    if (answer == Answer.Yes)
+                    {
+                        System.Console.Out.WriteLine("{0} agreed to marry {1}, they got married, and they lived happily ever after.", favoredWoman, currentMan);
+                        unmarriedWomen.Remove(favoredWoman);
+                    }
+                    else if (answer == Answer.Maybe)
+                    {
+                        System.Console.Out.WriteLine("{0} said that she would marry {1} but we aren't sure that she won't change her mind.", favoredWoman, currentMan);
+                    }
+                    else if (answer == Answer.No)
+                    {
+                        System.Console.Out.WriteLine("{0} outright rejected {1}", favoredWoman, currentMan);
+                    }
+                }
+                else
+                {
+                    System.Console.Out.WriteLine("{0} ran out of options", currentMan);
+                }
+                singleMen.RemoveAt(0); // remove the man that took his turn
+                // he might have been added to the end of the list
+            }
+
+            foreach(Person woman in unmarriedWomen.ToList())
+            {
+                if (woman.CurrentEngagement != null)
+                {
+                    System.Console.Out.WriteLine("{0} made up her mind and married {1} and they lived happily ever after.", woman, woman.CurrentEngagement);
+                    unmarriedWomen.Remove(woman);
+                    unmarriedMen.Remove(woman.CurrentEngagement);
+                }
+                else
+                {
+                    System.Console.Out.WriteLine("{0} remains unmarried.", woman);
+                }
+            }
+
+            foreach (Person man in unmarriedMen)
+            {
+                System.Console.Out.WriteLine("{0} remains unmarried.", man);
+            }
+
+            System.Console.Write("Press Enter...");
+            string waitForEnter = System.Console.ReadLine();
+        }
+
+        static void GetBackInTheGame(Person p, Person rejector)
+        {
+            singleMen.Add(p);
+            System.Console.Out.WriteLine("{0} was rejected by {1}, but got back in the game", p, rejector);
+        }
+    }
+}
